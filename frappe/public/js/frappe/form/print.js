9201f34165266084cba4ee8391daf8dbbf6736c8
KanchanChauhan <angel143.dr@gmail.com>
2019-09-03 01:17:06 +0530

    fix(print-preview): Default language set fix

--- frappe/public/js/frappe/form/print.js
+++ frappe/public/js/frappe/form/print.js
@@ -113,15 +113,10 @@ frappe.ui.form.PrintPreview = Class.extend({
 		this.preview();
 	},
 	set_default_print_language: function () {
- 		var print_format = this.get_print_format();
-
- 		if (print_format.default_print_language) {
- 			this.lang_code = print_format.default_print_language;
- 			this.language_sel.val(this.lang_code);
- 		} else {
-			this.language_sel.val(frappe.boot.lang);
-		}
- 	},
+		var print_format = this.get_print_format();
+		this.lang_code = print_format.default_print_language || this.frm.doc.language || frappe.boot.lang;
+		this.language_sel.val(this.lang_code);
+	},
 	multilingual_preview: function () {
 		var me = this;
 		if (this.is_old_style()) {
